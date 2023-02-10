1ecf5d8de9deaa2c274e3f7a581fab2790819aa2
Yousef <yousefelsisyy@gmail.com>
2023-02-10 13:27:19 -0400

    Add pubsub and draft of updating gameArray
    The code for updating gameArray is not done yet, but I wanted to
    see if pubsub works properly, and it does.

--- script.js
+++ script.js
@@ -2,6 +2,33 @@ function main() {
     
 }
 
+//events - a super-basic Javascript (publish subscribe) pattern
+
+var events = {
+  events: {},
+  on: function (eventName, fn) {
+    this.events[eventName] = this.events[eventName] || [];
+    this.events[eventName].push(fn);
+  },
+  off: function(eventName, fn) {
+    if (this.events[eventName]) {
+      for (var i = 0; i < this.events[eventName].length; i++) {
+        if (this.events[eventName][i] === fn) {
+          this.events[eventName].splice(i, 1);
+          break;
+        }
+      };
+    }
+  },
+  emit: function (eventName, data) {
+    if (this.events[eventName]) {
+      this.events[eventName].forEach(function(fn) {
+        fn(data);
+      });
+    }
+  }
+};
+
 let gameboard = (function() {
 
     //cache DOM
@@ -13,6 +40,10 @@ let gameboard = (function() {
     //When a box is clicked, its number will be stored in this value.
     let _currentBox = null;
 
+    function _render() {
+        events.emit('boxSelected', _currentBox);
+    }
+
     function addBoxListener(box) {
         box.addEventListener("click", _setClickedBoxNum);
     }
@@ -21,6 +52,7 @@ let gameboard = (function() {
     function _setClickedBoxNum() {
         let num = this.dataset.num;
         _currentBox = num
+        _render();
     }
 
     function getClickedBoxNum() {
@@ -37,8 +69,26 @@ let game = (function () {
     //Array to hold the value of each box on the gameboard.
     let _gameArray = [null, null, null, null, null, null, null, null, null]
 
-    //Assigns the number of the latest box to be clicked to chosenBox.
-    chosenBox = gameboard.getClickedBoxNum();
-})()
+    //bind events
+    events.on('boxSelected', _updateGameArray);
+
+    function _updateGameArray(chosenBox) {
+        _gameArray[chosenBox] = "X";
+        console.log(_gameArray);
+    }
+})();
+
+function createPlayer(name, symbol) {
+    return {
+        name: name,
+        symbol: symbol,
+        getSymbol() {
+            return symbol;
+        }
+    }
+}
+
+let player1 = createPlayer("player1", "X");
+let player2 = createPlayer("player2", "O");
 
 main();
