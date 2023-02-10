ba7a2611dc12779f9c496335f6e71d18f21d8ded
Yousef <yousefelsisyy@gmail.com>
2023-02-10 12:33:25 -0400

    Rearrange the organization of gameboard module

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 function main() {
-    gameboard.getClickedBoxNum();
+    
 }
 
 let gameboard = (function() {
 
     //cache DOM
-    let _boxes = document.querySelectorAll(".box");
+    let $boxes = document.querySelectorAll(".box");
+
+    //bind events
+    $boxes.forEach(addBoxListener);
+
+    function addBoxListener(box) {
+        box.addEventListener("click", getClickedBoxNum);
+    }
+
+    function getClickedBoxNum() {
+        let num = this.dataset.num;
+    }
 
     //Add event listener to every box of the gameboard and get its number.
-    let getClickedBoxNum = () => {
-        _boxes.forEach(addBoxListener);
-
-        function addBoxListener(box) {
-            box.addEventListener("click", getBoxNum);
-        }
-
-        function getBoxNum() {
-            let num = this.dataset.num;
-        }
-    };
 
     return {
-        getClickedBoxNum: getClickedBoxNum,
+        
     }
 })();
 
